8b1e51cf4b204d85f687e9ec9c80ffd40205d0ef
MoeFar95 <89990024+MoeFar95@users.noreply.github.com>
2021-12-07 22:42:38 -0500

    work done

---
+++ Main/routes/apiRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { readFromFile, readAndAppend, deleteNote } = require('../helper/fsUtils');
+const uuid = require('../helper/uuid');
+
+
+// GET route for retrieving all the notes
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
+
+// POST route  for submitting 
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        // Read JSON file and append new note
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error creating a new note');
+    }
+})
+
+//DELETE request
+router.delete('/notes/:id', (req, res) => {
+    console.log(req.params.id);
+    deleteNote(req.params.id, './db/db.json');
+    res.json('success');
+});
+
+// Export
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# MohamedNoteTaker
+# 11 Mohamed's Note Taking!
+
+## Your Task
+
+Hello, welcome to my note taker app. It was quite difficult but I managed to overcome the odds.
+
+
+## What it shows
+
+This is exactly what it looks like when you run the app.
+
+![Existing notes are listed in the left-hand column with empty fields on the right-hand side for the new note’s title and text.](./Assets/11-express-homework-demo-01.png)
+
+![Note titled “Balance accounts” reads, “Balance account books by end of day Monday,” with other notes listed on the left.](./Assets/11-express-homework-demo-02.png)
+
+
+## Difficulties
+
+What made it difficult was to use the heroku app and just backend in general.
